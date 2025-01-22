8019b41a207a24604ee922683373ca7e61542f23
kevin-code <kevincode26@gmail.com>
2025-01-22 09:38:47 +0000

    Add utils.h

---
+++ include/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "params.h"
+
+int maximum(int[]);
+int lireNombre(string, string);
+char lireChar(string, string);
+int lireNombreOuChar(string, string);
+int demanderCle();
+
+#endif
